e397299eb37543ebaf8b42c96f59b8da3eb54a78
Joe Parsons <parsonsj@email.arizona.edu>
2022-02-01 18:32:07 -0700

    Closes #16: Add environment-specific perforhamce & caching configuration overrides.

--- web/sites/default/settings.upstream.php
+++ web/sites/default/settings.upstream.php
@@ -33,3 +33,57 @@ if (file_exists(MIGRATION_DB_CONFIG_FILE_PATH)) {
     ];
   }
 }
+
+/**
+ * Default configuration for all Pantheon Environment requests.
+ *
+ * Settings in this block will be loaded for ALL requests (web and CLI) to
+ * Pantheon environments.
+ */
+if (defined('PANTHEON_ENVIRONMENT')) {
+  /**
+   * Redirect all http requests to a pantheonsite.io domain to https
+   */
+  if (php_sapi_name() !== 'cli') {
+    if (
+      preg_match('/.*\.pantheonsite\.io$/', $_SERVER['HTTP_HOST']) &&
+      (!isset($_SERVER['HTTP_USER_AGENT_HTTPS'])
+        || $_SERVER['HTTP_USER_AGENT_HTTPS'] !== 'ON')
+    ) {
+      header('HTTP/1.0 301 Moved Permanently');
+      header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+      exit();
+    }
+  }
+
+  /**
+   * Environment-specific performance and caching configuration overrides.
+   *
+   * @see https://pantheon.io/docs/drupal-cache/
+   */
+
+  // Performance settings for test and live environments.
+  if (in_array($_ENV['PANTHEON_ENVIRONMENT'], array('test', 'live'))) {
+    // Browser and proxy cache maximum age - 6 hours.
+    $config['system.performance']['cache']['page']['max_age'] = 21600;
+
+    // Aggregate CSS files - on.
+    $config['system.performance']['css']['preprocess'] = 1;
+
+    // Aggregate CSS files - on.
+    $config['system.performance']['js']['preprocess'] = 1;
+  }
+
+  // Performance settings for development environments, including multidev.
+  else {
+    // Browser and proxy cache maximum age - No caching.
+    $config['system.performance']['cache']['page']['max_age'] = 0;
+
+    // Aggregate CSS files - off.
+    $config['system.performance']['css']['preprocess'] = 0;
+
+    // Aggregate CSS files - off.
+    $config['system.performance']['js']['preprocess'] = 0;
+  }
+
+}
