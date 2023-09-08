e821f3bad8b5b708021f42aa362694c5b1fe0807
Chris Green <chrisgreen@arizona.edu>
2023-09-08 14:51:34 -0700

    Closes #185 & Closes #181 Update example Quicksilver scripts in light of new commands/options. (#186)
    Co-authored-by: Joe Parsons <471936+joeparsons@users.noreply.github.com>

--- web/private/scripts/drush_config_distro_update.php
+++ web/private/scripts/drush_config_distro_update.php
@@ -2,8 +2,7 @@
 
 // Ensure update mode is 'merge' and run config-distro-update.
 echo "Running Config Distro update...\n";
-passthru('drush -n -y state:set config_sync.update_mode 1 --input-format=integer');
-passthru('drush -n -y config-distro-update');
+passthru('drush -n -y config-distro-update --update-mode=1');
 echo "Config Distro update complete.\n";
 
 // Rebuild the cache.
